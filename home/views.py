cacfc6f7c0a31fee1bd2e6c791b468808697bce3
user.email <573780734@qq.com>
2019-06-20 09:43:02 +0800

    email

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from yongjie.form import PriceForm
 from django.http import HttpResponseRedirect,HttpResponse
 import json
+from django.core.mail import send_mail
 
 def get_list_common_data(request, all_list):
     paginator = Paginator(all_list, settings.EACH_PAGE_BLOGS_NUMBER)
@@ -280,6 +281,16 @@ def price(request):
 			mobile=mobile,
 		)
 		if price_obj:
+			# 发送邮件
+			if sex == '1':
+				sex='男'
+			else:
+				sex='女'
+
+			msg = '新的报价信息：</br>报价人姓名：%s </br>性别:%s </br>手机号:%s </br>车间面积:%s m² <a href "http://47.94.235.194/admin/home/price/" >查看详细详情</a>'%(name,sex,mobile,area)
+			#msg = "hellotestofemail "
+			send_mail('提交成功', '有人通过网站提交了报价，请注意查收.', '2860889124@qq.com',
+					  ['573780734@qq.com'], html_message=msg)
 			return HttpResponse(json.dumps({'status':'success'}))
 	else:
 		return HttpResponse(json.dumps({'status':'fail','msg':"请您正确填写表单"}))
--- yongjie/settings.py
+++ yongjie/settings.py
@@ -193,6 +193,14 @@ EACH_PAGE_BLOGS_NUMBER=3
 EACH_PAGE_SOLUTIONS_NUMBER=4
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = False   #是否使用TLS安全传输协议(用于在两个通信应用程序之间提供保密性和数据完整性。)
+EMAIL_USE_SSL = True    #是否使用SSL加密，qq企业邮箱要求使用
+EMAIL_HOST = 'smtp.qq.com'   #发送邮件的邮箱 的 SMTP服务器，这里用了163邮箱
+EMAIL_PORT = 465     #发件箱的SMTP服务器端口
+EMAIL_HOST_USER = '2860889124@qq.com'    #发送邮件的邮箱地址
+EMAIL_HOST_PASSWORD = 'mwbtyflwdbtkdchb'         #发送邮件的邮箱密码(这里使用的是授权码)
+DEFAULT_FROM_EMAIL = '顺尚 <2860889124@qq.com>'
 
 # 日志文件
 # LOGGING = {
